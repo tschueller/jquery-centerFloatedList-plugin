9a0c039a3314b6384c104b61da44ff1951d3c1a2
Thorsten Schüller <t.schueller@iplabs.de>
2012-12-18 21:03:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+jQuery centerFloatedList plugin
+===============================
+
+Description
+-----------
+CenterFloatedList is a jQuery plugin that centers a list with floated items, as a picture gallery. 
+
+Required jQuery 1.4.2 or higher.
+
+Demo: [http://schueller.me/projects/jquery-plugins/](http://schueller.me/projects/jquery-plugins/)
+
+Usage
+-----
+
+	$('#theListToCenter').centerFloatedList();
+
+
+Images
+-------
+
+## Before the centering ##
+![uncentered list](./images/centerFloatedList1.png)	
+
+
+## After the centering ##
+
+![centered list](./images/centerFloatedList2.png)	
---
+++ jquery.centerFloatedList.js
@@ -0,0 +1,40 @@
+/*!
+ * jQuery centerFloatedList Plugin v1.0.1 
+ * Centers a list with floated list items.
+ * Copyright (c) 2012, Thorsten Schüller 
+ * http://schueller.me/projects/
+ * Licensed under the MIT license.
+ */
+
+(function( $ ) 
+{
+    $.fn.centerFloatedList = function() 
+    {
+        return this.each(function() 
+        {
+			var $this = $(this),
+			
+			centerTheList = function()
+			{
+				var items, itemsCount, itemWidth, containerWidth,
+					itemsInRowCount, emptySpace;
+					
+				items = $this.find("li");
+				itemsCount = items.size(); 
+				if (itemsCount) 
+				{
+					itemWidth = items.first().outerWidth(true);
+					containerWidth = $this.innerWidth();
+					itemsInRowCount = Math.min(itemsCount, 
+							Math.floor(containerWidth / itemWidth));
+					emptySpace = containerWidth - itemsInRowCount * itemWidth;
+					
+					$this.css("paddingLeft", Math.floor(emptySpace / 2) + "px");			
+				}
+			}
+			
+			centerTheList();
+			$(window).bind('resize', centerTheList);
+        });        
+    };
+})( jQuery );
